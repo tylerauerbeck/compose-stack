50d5e0f54fdb969d0ba6a6cefc940f9a15f784ac
Nicole Hubbard <code@nicole.dev>
2023-08-14 17:26:38 +0000

    fix compose stack with new events package thats in some apis

--- nats/scripts/stream_init.sh
+++ nats/scripts/stream_init.sh
@@ -1,2 +1,1 @@
-nats -s nats:4222 --creds=/api-creds/user.creds stream add infratographer-changes --subjects="com.infratographer.>" --storage=file --replicas=1 --retention=limits --discard=old --max-msg
-s=-1 --max-msgs-per-subject=-1 --max-bytes=-1 --max-age=-1 --max-msg-size=-1 --dupe-window=2m0s --allow-rollup --no-deny-delete --no-deny-purge
+nats -s nats:4222 --creds=/api-creds/user.creds stream add infratographer-changes --subjects="com.infratographer.>" --storage=file --replicas=1 --retention=limits --discard=old --max-msgs=-1 --max-msgs-per-subject=-1 --max-bytes=-1 --max-age=-1 --max-msg-size=-1 --dupe-window=2m0s --allow-rollup --no-deny-delete --no-deny-purge
